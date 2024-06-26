a2e7a9058574ce487ccf07fdcb35055491468c21
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-06-26 16:40:00 +0200

    using function

--- src/main/java/com/example/englishacademyspringbootfunction/AzureFunction.java
+++ src/main/java/com/example/englishacademyspringbootfunction/AzureFunction.java
@@ -1,24 +1,26 @@
 package com.example.englishacademyspringbootfunction;
 
-import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.function.context.FunctionCatalog;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class AzureFunction {
 
+    private FunctionCatalog functionCatalog;
+
     @FunctionName("hello")
-    public String plainBean(
+    public HttpResponseMessage plainBean(
             @HttpTrigger(
                     name = "req",
                     methods = {HttpMethod.GET},
@@ -26,7 +28,14 @@ public class AzureFunction {
             ) HttpRequestMessage<Optional<String>> request,
             ExecutionContext context) {
         log.info("Received request");
-        return "hello world from Azure function with github Actions :)";
+
+        log.info("Received request for uppercase function");
+
+        String input = request.getBody().orElse("");
+        Function<String, String> uppercaseFunction = functionCatalog.lookup(Function.class, "uppercase");
+        String result = uppercaseFunction.apply(input);
+
+        return request.createResponseBuilder(HttpStatus.OK).body(result).build();
 
     }
 
