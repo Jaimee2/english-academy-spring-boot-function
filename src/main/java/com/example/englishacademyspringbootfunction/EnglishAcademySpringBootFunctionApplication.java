e4806187f88871c74549645bb09037e8f0df5d25
Jaime <jaimehiguerasmedina98@gmail.com>
2024-07-01 13:16:24 +0200

    deleted post construct

--- src/main/java/com/example/englishacademyspringbootfunction/EnglishAcademySpringBootFunctionApplication.java
+++ src/main/java/com/example/englishacademyspringbootfunction/EnglishAcademySpringBootFunctionApplication.java
@@ -21,33 +21,4 @@ public class EnglishAcademySpringBootFunctionApplication {
         SpringApplication.run(EnglishAcademySpringBootFunctionApplication.class, args);
     }
 
-    @PostConstruct
-    void loadData() {
-        log.info("***************************************************************");
-
-        Student student = Student.builder()
-                .firstName("Jaime without id")
-                .lastName("Doe")
-                .address("123 Main St")
-                .parentPhone("123-456-7890")
-                .parentEmail("parent@example.com")
-                .siblings("None")
-                .notes("Interested in football")
-                .books("yes")
-                .payment("cash")
-                .studentStatus("new")
-                .build();
-
-        studentsRepository.save(student);
-
-        List<Student> studentList = studentsRepository.findAll();
-        studentList.forEach(System.out::println);
-
-        System.out.println("------------------------------------------------------");
-        List<Student> students = studentsRepository.findAll();
-        System.out.println(students);
-        log.info("***************************************************************");
-    }
-
-
 }
