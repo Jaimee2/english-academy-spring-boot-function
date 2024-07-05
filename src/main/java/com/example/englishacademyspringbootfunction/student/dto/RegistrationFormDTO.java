b94ab52ce25cfe8ef0c7e82f641c8746ed38950b
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-07-05 14:52:53 +0200

    init modify student register

--- src/main/java/com/example/englishacademyspringbootfunction/student/dto/RegistrationFormDTO.java
+++ src/main/java/com/example/englishacademyspringbootfunction/student/dto/RegistrationFormDTO.java
@@ -2,14 +2,12 @@ package com.example.englishacademyspringbootfunction.student.dto;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 public class RegistrationFormDTO {
 
     private String firstName;
     private String lastName;
-    private LocalDate birthDate;
+    private String birthDate;
     private String address;
     private String parentPhone;
     private String parentEmail;
